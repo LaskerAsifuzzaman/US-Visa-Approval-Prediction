bec32a684dd3ad77087dc136b4103c2d832dd372
LaskerAsifuzzaman <asifuzzaman.lasker@yahoo.in>
2026-02-03 08:01:44 +0530

    Till now data access file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Asifuzzaman Lasker",
+    author_email="asifuzzaman.lasker@yahoo.in",
+    packages=find_packages()
+)
